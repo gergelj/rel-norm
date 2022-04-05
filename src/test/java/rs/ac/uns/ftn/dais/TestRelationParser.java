c15bef4cc411bc728587111d1489462e71aefeac
gregvader <kgerg13@gmail.com>
2022-04-05 20:17:02 +0200

    test: relation parser

---
+++ src/test/java/rs/ac/uns/ftn/dais/TestRelationParser.java
@@ -0,0 +1,55 @@
+package rs.ac.uns.ftn.dais;
+
+import org.junit.Test;
+import rs.ac.uns.ftn.dais.domain.FunctionalDependency;
+import rs.ac.uns.ftn.dais.domain.FunctionalDependencySet;
+import rs.ac.uns.ftn.dais.domain.LabelSet;
+import rs.ac.uns.ftn.dais.domain.Relation;
+import rs.ac.uns.ftn.dais.domain.RelationParser;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TestRelationParser {
+
+    private final RelationParser parser = new RelationParser();
+
+    @Test
+    public void relationParserTest1() {
+        try {
+            Relation loaded = parser.parse("src/test/resources/task1.json");
+
+            FunctionalDependencySet fdset = new FunctionalDependencySet();
+            fdset.add(new FunctionalDependency(new String[]{"E", "F"}, new String[]{"C"}));
+            fdset.add(new FunctionalDependency(new String[]{"C"}, new String[]{"E"}));
+            LabelSet labels = new LabelSet(new String[]{"E", "F", "C"});
+            Relation expected = new Relation("N", labels, fdset);
+            assertEquals(expected, loaded);
+
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void relationParserTest2() {
+        try {
+            Relation loaded = parser.parse("src/test/resources/task2.json");
+
+            FunctionalDependencySet fdset = new FunctionalDependencySet();
+            fdset.add(new FunctionalDependency(new String[]{"R", "C", "X"}, new String[]{"A", "L"}));
+            fdset.add(new FunctionalDependency(new String[]{"X"}, new String[]{"V"}));
+            fdset.add(new FunctionalDependency(new String[]{"X"}, new String[]{"X", "G", "R"}));
+            fdset.add(new FunctionalDependency(new String[]{"G"}, new String[]{"G", "R"}));
+            fdset.add(new FunctionalDependency(new String[]{"V"}, new String[]{"L"}));
+            LabelSet labels = new LabelSet(new String[]{"A", "R", "C", "X", "V", "G", "L", "M"});
+            Relation expected = new Relation("N", labels, fdset);
+            assertEquals(expected, loaded);
+
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+}
